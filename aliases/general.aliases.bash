62353e47068f7f629fc211383eaa171ad7a97acf
Maxim Burgerhout <maxim@wzzrd.com>
2011-03-07 23:33:05 +0100

    Moved the ls aliases to a separate file I nicked from /etc/profile.d/colorls.sh on a Fedora system and added the remaining aliases from general.aliases.bash to it. Removed some more exotic aliases (edit, page). Added a space after the sudo command in the _ alias to allow interpreting the command after the _ as an alias as well. Cleaned up the help function.

--- aliases/general.aliases.bash
+++ aliases/general.aliases.bash
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# List directory contents
-alias sl=ls
-alias ls='ls -G'        # Compact view, show colors
-alias la='ls -AF'       # Compact view, show hidden
-alias ll='ls -al'
-alias l='ls -a'
-alias l1='ls -1'
-
-alias _="sudo"
-
-alias edit="$EDITOR"
-alias page="$PAGER"
+alias _="sudo "
 
 alias ..='cd ..'        # Go up one directory
 alias ...='cd ../..'    # Go up two directories
@@ -28,14 +17,11 @@ alias d='dirs -v'
 function aliases-help() {
   echo "Generic Alias Usage"
   echo
-  echo "  sl     = ls"
-  echo "  ls     = ls -G"
-  echo "  la     = ls -AF"
-  echo "  ll     = ls -al"
-  echo "  l      = ls -a"
+  echo "  _      = sudo "
   echo "  ..     = cd .."
   echo "  ...    = cd ../.."
   echo "  -      = cd -"
+  echo "  tree   = show directory tree"
   echo "  md     = mkdir -p"
   echo "  rd     = rmdir"
   echo "  d      = dirs -v"
