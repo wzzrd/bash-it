178fe943305d158e95e7324bb46123b3bf669977
Maxim Burgerhout <maxim@wzzrd.com>
2011-03-07 23:04:07 +0100

    Removed some of the more exotic aliases here, like piano (should be a plugin), irc (is an existing command) and h (conflicts with the heroku alias somewhere).

--- aliases/general.aliases.bash
+++ aliases/general.aliases.bash
@@ -10,35 +10,19 @@ alias l1='ls -1'
 
 alias _="sudo"
 
-alias c='clear'
-alias k='clear'
-
 alias edit="$EDITOR"
 alias page="$PAGER"
-
-alias q="exit"
-
-alias irc="$IRC_CLIENT"
-
-alias rb="ruby"
-
-# Pianobar can be found here: http://github.com/PromyLOPh/pianobar/
-
-alias piano="pianobar"
 
 alias ..='cd ..'        # Go up one directory
 alias ...='cd ../..'    # Go up two directories
 alias -- -="cd -"       # Go back
 
-# Shell History
-alias h='history'
-
 # Tree
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 
 # Directory
-alias	md='mkdir -p'
-alias	rd=rmdir
+alias md='mkdir -p'
+alias rd=rmdir
 alias d='dirs -v'
 
 function aliases-help() {
@@ -49,18 +33,11 @@ function aliases-help() {
   echo "  la     = ls -AF"
   echo "  ll     = ls -al"
   echo "  l      = ls -a"
-  echo "  c/k    = clear"
   echo "  ..     = cd .."
   echo "  ...    = cd ../.."
   echo "  -      = cd -"
-  echo "  h      = history"
   echo "  md     = mkdir -p"
   echo "  rd     = rmdir"
   echo "  d      = dirs -v"
-  echo "  editor = $EDITOR"
-  echo "  pager  = $PAGER"
-  echo "  piano  = pianobar"
-  echo "  q      = exit"
-  echo "  irc    = $IRC_CLIENT"
   echo 
 }
